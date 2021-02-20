57d526038bb0d469c9c447ca2c3277bfa3d4f50e
djhardy614 <78882124+djhardy614@users.noreply.github.com>
2021-02-20 16:10:14 +0000

    Update Player match history api.py

--- Player match history api.py
+++ Player match history api.py
@@ -1,10 +1,11 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 import json
 from time import sleep
-
+f = open('C:/Users/djhar/Documents/key.txt', 'r')
+key = f.read()
 headers = {
     # Request headers
-    'Ocp-Apim-Subscription-Key': '',
+    'Ocp-Apim-Subscription-Key': key,
 }
 
 params = urllib.parse.urlencode({
@@ -16,7 +17,6 @@ try:
     conn.request("GET", "/stats/h5/players/budbudhardy/matches?modes=custom", "{body}", headers)
     response = conn.getresponse()
     my_bytes_value = response.read()
-    # print(data)
     conn.close()
 except Exception as e:
     print("[Errno {0}] {1}".format(e.errno, e.strerror))
@@ -25,45 +25,55 @@ my_json = my_bytes_value.decode('utf8').replace("'", '"')
 
 data = json.loads(my_json)
 s = json.dumps(data, indent=4, sort_keys=True)
-# print(data['Results'])
-# print(s)
 match_id_list = []
-# for info in data['Results']:
-#     match_id= info['Id']['MatchId']
-#     match_id_list.append(match_id)
+for info in data['Results']:
+    match_id= info['Id']['MatchId']
+    match_id_list.append(match_id)
+player_kills_count = []
+player_death_count = []
+player_assist_count = []
 for info in data['Results']:
     player_kills = info['Players'][0]['TotalKills']
-    
-    print(player_info)
-# print(match_id_list)
+    player_kills_count.append(player_kills)
+    player_deaths = info['Players'][0]['TotalDeaths']
+    player_death_count.append(player_deaths)
+    player_assist = info['Players'][0]['TotalAssists']
+    player_assist_count.append(player_assist)
+gamertag = data['Results']
+print(gamertag[0]['Players'][0]['Player']['Gamertag'])
+player_kda = (sum(player_kills_count) + (1/3*(sum(player_assist_count))))/sum(player_death_count)
+print(round(player_kda,3))
+print(sum(player_kills_count))
+print(sum(player_death_count))
+print(sum(player_assist_count))
+print('-' * 30)
 
-# for match_id in match_id_list[0:5]:
+for match_id in match_id_list[0:5]:
 
-#     sleep(5)
+    sleep(5)
 
-#     try:
-#         conn = http.client.HTTPSConnection('www.haloapi.com')
-#         conn.request("GET", f"/stats/h5/custom/matches/{match_id}", "{body}", headers) 
-#         response = conn.getresponse()
-#         my_bytes_value = response.read()
-#         # print(data)
-#         conn.close()
-#     except Exception as e:
-#         print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    try:
+        conn = http.client.HTTPSConnection('www.haloapi.com')
+        conn.request("GET", f"/stats/h5/custom/matches/{match_id}", "{body}", headers) 
+        response = conn.getresponse()
+        my_bytes_value = response.read()
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
-#     my_json = my_bytes_value.decode('utf8').replace("'", '"')
+    my_json = my_bytes_value.decode('utf8').replace("'", '"')
 
-#     data = json.loads(my_json)
-#     # print(data)
-#     s = json.dumps(data, indent=4, sort_keys=True)
-#     for info in data['PlayerStats']:       #to select relevant info, add in dictionary key 
-#         name = info['Player']['Gamertag']
-#         totalkills = info['TotalKills']
-#         totalassists = info['TotalAssists']
-#         totaldeaths = info['TotalDeaths']
-#         weapondamage = info['TotalWeaponDamage']
-#         shoulderbash = info['TotalShoulderBashDamage']
-#         totalheadshot = info['TotalHeadshots']
-#         kda = (totalkills + (totalassists * 1/3))/(totaldeaths)
-#         print(name, totalkills, totalassists, totaldeaths, round(kda,2))
+    data = json.loads(my_json)
+    s = json.dumps(data, indent=4, sort_keys=True)
+    for info in data['PlayerStats']:       #to select relevant info, add in dictionary key 
+        name = info['Player']['Gamertag']
+        totalkills = info['TotalKills']
+        totalassists = info['TotalAssists']
+        totaldeaths = info['TotalDeaths']
+        weapondamage = info['TotalWeaponDamage']
+        shoulderbash = info['TotalShoulderBashDamage']
+        totalheadshot = info['TotalHeadshots']
+        kda = (totalkills + (totalassists * 1/3))/(totaldeaths)
+        print(name, totalkills, totalassists, totaldeaths, round(kda,2))
+    print('-'*30)
 
